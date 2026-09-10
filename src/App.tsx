44625809c2529ace6333914fa0509bcd812cd984
soniaetuhoko <s.etuhoko@alustudent.com>
2026-09-11 01:56:28 +0200

    Components with both headings and paragraph

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from './MyComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
