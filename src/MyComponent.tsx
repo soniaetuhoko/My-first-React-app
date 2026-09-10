e5f66ddeddcdd18284e64bf45d9941ffff1cc32e
soniaetuhoko <s.etuhoko@alustudent.com>
2026-09-11 01:54:41 +0200

    Created my components with headings

---
+++ src/MyComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MyComponent.css';
+
+const MyComponent: React.FC = () => {
+  return (
+    <div className="my-component">
+      <h1>Hello from MyComponent!</h1>
+      <p>This is a paragraph of text within my component.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
